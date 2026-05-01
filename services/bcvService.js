73559ddc83c7ec0a1472807f53f5f40025c75bad
Darwin Ramirez <darwinsgv@gmail.com>
2026-04-30 22:44:47 -0500

    fix merge conflicts

--- services/bcvService.js
+++ services/bcvService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import cheerio from "cheerio";
+//import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 
 const URL = "https://www.bcv.org.ve/";
 //const URL = "https://www.bcv.org.ve/tasas-informativas-sistema-bancario";
